dd168739909c3fd615bb6a62f138f41285ec5280
clde-ber <claire.deberny@gmail.com>
2020-11-30 19:55:20 +0100

    influxdb

---
+++ srcs/InfluxDB/docker-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# -g sets global directives
+# a daemon is a computer program that runs as a background process, rather than being under the direct control of an interactive user.
+# daemon off directive prevents the container for stopping right after the command is executed, which would be the standard behavior of a conainter.
+rc-update add influxdb default
+rc boot
+rc-service influxdb start
+sleep infinity & wait
+bash
