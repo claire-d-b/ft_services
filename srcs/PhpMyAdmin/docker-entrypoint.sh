096b1273cb2454d0549be42c963541ecf2c83a04
clde-ber <claire.deberny@gmail.com>
2020-11-30 16:30:06 +0100

    ftps & php services running

--- srcs/PhpMyAdmin/docker-entrypoint.sh
+++ srcs/PhpMyAdmin/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-rc-update add vsftpd default
+rc-update add php-fpm7 default
 rc boot
-rc-service vsftpd start
+rc-service php-fpm7 start
 # -g sets global directives
 # a daemon is a computer program that runs as a background process, rather than being under the direct control of an interactive user.
 # daemon off directive prevents the container for stopping right after the command is executed, which would be the standard behavior of a conainter.
