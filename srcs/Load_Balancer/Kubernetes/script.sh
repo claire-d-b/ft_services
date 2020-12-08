65e8e70756646b95e84ad7f97a7aba272a823365
clde-ber <claire.deberny@gmail.com>
2020-12-08 15:49:45 +0100

    wordpress bash

--- srcs/Load_Balancer/Kubernetes/script.sh
+++ srcs/Load_Balancer/Kubernetes/script.sh
@@ -20,19 +20,19 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list
 apt-get update
 apt-get install -y kubectl
-#cd ../../Nginx && docker build -t image_nginx .
-#cd ../FTPS && docker build -t image_vsftpd .
-#cd ../Grafana && docker build -t image_grafana .
-#cd ../PhpMyAdmin && docker build -t image_phpmyadmin .
-cd ../../WordPress && docker build -t image_wordpress .
-#cd ../InfluxDB && docker build -t image_influxdb .
-#cd ../MySQL && docker build -t image_mysql .
-#cd ../Nginx && docker run -tid --name nginx image_nginx 
-#cd ../FTPS && docker run -tid --name vsftpd image_vsftpd
-#cd ../Grafana && docker run -tid --name grafana image_grafana
-#cd ../PhpMyAdmin && docker run -tid --name phpmyadmin image_phpmyadmin
+cd ../../Nginx && docker build -t image_nginx .
+cd ../FTPS && docker build -t image_vsftpd .
+cd ../Grafana && docker build -t image_grafana .
+cd ../PhpMyAdmin && docker build -t image_phpmyadmin .
+cd ../WordPress/ && docker build -t image_wordpress .
+cd ../InfluxDB && docker build -t image_influxdb .
+cd ../MySQL && docker build -t image_mysql .
+cd ../Nginx && docker run -tid --name nginx image_nginx 
+cd ../FTPS && docker run -tid --name vsftpd image_vsftpd
+cd ../Grafana && docker run -tid --name grafana image_grafana
+cd ../PhpMyAdmin && docker run -tid --name phpmyadmin image_phpmyadmin
 cd ../WordPress && docker run -tid --name wordpress image_wordpress
-#cd ../InfluxDB && docker run -tid --name influxdb image_influxdb
-#cd ../MySQL && docker run -tid --name mysql image_mysql
-#cd ../Load_Balancer/Kubernetes
+cd ../InfluxDB && docker run -tid --name influxdb image_influxdb
+cd ../MySQL && docker run -tid --name mysql image_mysql
+cd ../Load_Balancer/Kubernetes
 bash
