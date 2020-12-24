d5895daf619032a026e3c993bfd5b8ee2e646a05
clde-ber <clde-ber@student.42.fr>
2020-12-24 10:12:51 +0100

    pods ready & deployments running

--- srcs/Grafana/docker-entrypoint.sh
+++ srcs/Grafana/docker-entrypoint.sh
@@ -2,5 +2,8 @@
 # -g sets global directives
 # a daemon is a computer program that runs as a background process, rather than being under the direct control of an interactive user.
 # daemon off directive prevents the container for stopping right after the command is executed, which would be the standard behavior of a conainter.
+rc-update add grafana default
+rc boot
+rc-service grafana start
 sleep infinity & wait
 bash
--- srcs/Load_Balancer/Kubernetes/script.sh
+++ srcs/Load_Balancer/Kubernetes/script.sh
@@ -31,7 +31,7 @@ apt-get -y update #&& apt-get -y upgrade
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
 # On first install only
-kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+#kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 cat <<EOF | kubectl create -f -
 apiVersion: v1
 kind: ConfigMap
