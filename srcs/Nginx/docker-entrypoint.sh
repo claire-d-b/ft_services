8b73a141bc311b8ed7445230edbe21eaf5ed95fa
clde-ber <clde-ber@student.42.fr>
2021-01-24 18:47:34 +0100

    nginx

--- srcs/Nginx/docker-entrypoint.sh
+++ srcs/Nginx/docker-entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
-if [ "$AUTOINDEX" = "on" ] ;
-then mv srcs/default_nginx_ai_on /etc/nginx/sites-available/default ;
-else mv srcs/default_nginx_ai_off /etc/nginx/sites-available/default ;
-fi
-rm -rf /etc/nginx/sites-enabled/default
-ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
+mv srcs/nginx.conf /etc/nginx/nginx.conf
 rc-update add nginx default
 openrc boot
 rc-service nginx start
