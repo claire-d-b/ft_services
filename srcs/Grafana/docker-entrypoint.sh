ab3cb86d424356472294372262975127765de0ec
clde-ber <clde-ber@student.42.fr>
2021-01-27 13:14:18 +0100

    grafana service

--- srcs/Grafana/docker-entrypoint.sh
+++ srcs/Grafana/docker-entrypoint.sh
@@ -2,8 +2,6 @@
 # -g sets global directives
 # a daemon is a computer program that runs as a background process, rather than being under the direct control of an interactive user.
 # daemon off directive prevents the container for stopping right after the command is executed, which would be the standard behavior of a conainter.
-rc-update add grafana default
-openrc boot
-rc-service grafana start
+/usr/sbin/grafana-server --config=/etc/grafana.ini --homepath=/usr/share/grafana
 sleep infinity & wait
 bash
