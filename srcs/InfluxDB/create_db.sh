cecdd3f0dd579dc68f877cd81ae7598e4eb78fae
clde-ber <clde-ber@student.42.fr>
2021-02-07 08:58:09 +0100

    wp site & users created & influx connexion influxdb:8086

--- srcs/InfluxDB/create_db.sh
+++ srcs/InfluxDB/create_db.sh
@@ -2,3 +2,4 @@
 sleep 20
 echo "CREATE DATABASE influxdb;" | influx -username grafana
 echo "CREATE USER grafana WITH PASSWORD '' WITH ALL PRIVILEGES;" | influx -username grafana
+use influxdb | influx -username grafana
