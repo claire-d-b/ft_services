994bb2595916567d4a46b316945442670c414300
clde-ber <clde-ber@student.42.fr>
2021-02-26 15:11:46 +0100

    unused info removed - grafana + pw change

--- srcs/Grafana/change_pw.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-until grafana-cli
-do
-  echo ""
-done
-
-grafana-cli --config=/etc/grafana.ini --homepath=/usr/share/grafana admin reset-admin-password clde-ber@student.42.fr
--- srcs/Grafana/docker-entrypoint.sh
+++ srcs/Grafana/docker-entrypoint.sh
@@ -9,13 +9,11 @@ sed -i 's+;name = grafana+name = influxdb+g' etc/grafana.ini
 sed -i 's+;user = root+user = grafana+g' etc/grafana.ini
 sed -i 's+;password =+password =+g' etc/grafana.ini
 sed -i 's+;admin_user = admin+admin_user = admin+g' etc/grafana.ini
-sed -i 's+;admin_password = admin+admin_password = clde-ber@student.42.fr+g' etc/grafana.ini
+sed -i 's+;admin_password = admin+admin_password = grafana+g' etc/grafana.ini
 sed -i 's+;secret_key+secret_key+g' etc/grafana.ini
 sed -i 's+;provisioning = conf/provisioning+provisioning = /usr/share/grafana/conf/provisioning+g' etc/grafana.ini
 sed -i 's+GRAFANA_HOME=/var/lib/grafana+GRAFANA_HOME=/usr/share/grafana+g' etc/conf.d/grafana
 sed -i 's+GRAFANA_OPTS="-config /etc/grafana.ini -homepath /usr/share/grafana+GRAFANA_OPTS="-config /usr/share/grafana/conf/grafana.ini -homepath /usr/share/grafana+g' etc/conf.d/grafana
-#rm -rf /usr/share/grafana/conf/sample.ini
-#rm -rf /usr/share/grafana/conf/defaults.ini
 mv etc/grafana.ini /usr/share/grafana/conf/
 nohup /usr/sbin/grafana-server --config=/usr/share/grafana/conf/grafana.ini --homepath=/usr/share/grafana 0</dev/null
 sleep infinity & wait
