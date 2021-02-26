eb8a86091711e26e76894cf91313850c0106f36a
clde-ber <clde-ber@student.42.fr>
2021-02-26 10:41:57 +0100

    livenessprobe & persistentvolume

--- setup.sh
+++ setup.sh
@@ -13,11 +13,7 @@ find $(pwd)/srcs -type f -exec \
 kubectl get configmap kube-proxy -n kube-system -o yaml | sed -e "s/strictARP: false/strictARP: true/" | kubectl apply -f - -n kube-system
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
-# On first install only
-#if [ -e $metallb ] ;
-#else
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-#fi
 kubectl apply -f ./srcs/config.yaml
 
 docker build -t image-nginx ./srcs/Nginx/
